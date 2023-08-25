3c37a27503907e95104f7804e0b0ff47f6a258cc
murilosccp <murilosantosdonascimento1992@gmail.com>
2023-08-25 11:27:56 -0300

    Desafio 52 sendo feito

---
+++ Python/Mundo 2/Aula 13/desafio52.py
@@ -0,0 +1,9 @@
+print("É Primo ou Não?")
+num = int(input("Digite um número: "))
+
+if num <= 1:
+        print("Você digitou o núemro {}, e ele não é um número primo.".format(num))
+
+for p in range(1, 11):
+    if num % num == 0 and num :
+        print("Você digitou o núemro {}, e ele não é um número primo.".format(num))
