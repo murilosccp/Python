bd3823c7b16a5e4b1d4ac098bf8d7a5a6b8c242e
murilosccp <murilosantosdonascimento1992@gmail.com>
2023-09-08 12:58:25 -0300

    Alterações Ex 53

--- Python/Mundo 2/Aula 13/desafio53.py
+++ Python/Mundo 2/Aula 13/desafio53.py
@@ -1,9 +1,12 @@
+print("?PALÍNDROMO OU NÃO?")
 frase = str(input("Digite uma frase: ")).strip().lower()
-word = frase.split()
-juntar = ''.join(word)
-inv = juntar[::-1]
-print('''O inverso de {} é {}'''.format(juntar, inv))
-if inv == juntar:
+palavras = frase.split()
+união = ''.join(palavras)
+inv = ''
+for caracter in range(len(união) -1, -1, -1):
+    inv += união[caracter]
+print('''O inverso de {} é {}'''.format(união, inv))
+if inv == união:
     print("""A frase é um Palíndromo HAHA""")
 else:
     print("""A frase NÃO é um Palíndromo :(""")
